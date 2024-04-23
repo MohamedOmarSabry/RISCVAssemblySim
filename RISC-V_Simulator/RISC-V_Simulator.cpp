dfaadf5f47340da74cf4620eac42d2a942c88a7b
MohamedOmarSabry <momo12320@aucegypt.edu>
2024-04-23 22:46:13 +0200

    added string to binary and string to hexa and binary to decimal and started on print

--- RISC-V_Simulator/RISC-V_Simulator.cpp
+++ RISC-V_Simulator/RISC-V_Simulator.cpp
@@ -1,8 +1,104 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
+map<string, string> registers;
+string Stringtobinary(string num) //working as intended
+{
+	long long r;
+	bool n = 0;
+	r = stoll(num);
+	if (r < 0)
+	{
+		n = 1;
+		num.erase(0, 1);
+		r = stoll(num);
+	}
+	string binary = "";
+	for (int i = 31; i >= 0; i--) {
+		int k = r >> i;
+		if (k & 1)
+			binary+='1';
+		else
+			binary+='0';
+	}
+	if (n)
+	{
+		int b = binary.length();
+		int i;
+		for (i = b - 1; i >= 0; i--)
+			if (binary[i] == '1')
+				break;
+		if (i == -1)
+			return '1' + binary;
+		for (int k = i - 1; k >= 0; k--)
+		{
+			if (binary[k] == '1')
+				binary[k] = '0';
+			else
+				binary[k] = '1';
+		}
+	}
+
+	return binary;
+}
+string Stringtohexa(string value) //works
+{
+	string binnum = Stringtobinary(value);
+	//cout << binnum << endl;
+	char hexa[8];
+	int temp;
+	long int i = 0, j = 0;
+	while (binnum[i])
+	{
+		binnum[i] = binnum[i] - 48;
+		i++;
+	}
+	i--;
+	while (i - 2 >= 0)
+	{
+		temp = binnum[i - 3] * 8 + binnum[i - 2] * 4 + binnum[i - 1] * 2 + binnum[i];
+		if (temp > 9)
+			hexa[j++] = temp + 55;
+		else
+			hexa[j++] = temp + 48;
+		i = i - 4;
+	}
+	if (i == 1)
+		hexa[j] = binnum[i - 1] * 2 + binnum[i] + 48;
+	else if (i == 0)
+		hexa[j] = binnum[i] + 48;
+	else
+		--j;
+	string s = "";
+	while (j >= 0)
+	{
+		s += hexa[j--];
+	}
+	return s;
+}
+int binaryToDecimal(string n) //works
+{
+	string num = n;
+	int dec_value = 0;
+	int base = 1;
+	int len = num.length();
+	for (int i = len - 1; i >= 0; i--) {
+		if (num[i] == '1')
+			dec_value += base;
+		base = base * 2;
+	}
+	return dec_value;
+}
+void PrintMem_Reg()
+{
+
+}
 int main()
 {
-    cout << "Hello World!\n";
+	cout<< Stringtohexa("2147483647")<<endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
